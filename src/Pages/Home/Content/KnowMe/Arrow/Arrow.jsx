81d1935295bd333c2b89338fb748c1ef2940324c
Smael Nicolas <nico.smael@gmail.com>
2022-03-04 16:07:25 -0300

    Basic Structure

---
+++ src/Pages/Home/Content/KnowMe/Arrow/Arrow.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Arrow() {
+	return <div>Arrow</div>;
+}
+
+export default Arrow;
---
+++ src/Pages/Home/Content/KnowMe/KnowMe.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function KnowMe() {
+	return <div>KnowMe</div>;
+}
+
+export default KnowMe;
