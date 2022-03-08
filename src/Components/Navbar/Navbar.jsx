6a0459e9f5bb2a6ebeef2b42897873330f18dec1
Smael Nicolas <nico.smael@gmail.com>
2022-03-08 18:14:27 -0300

    navbar base

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+import NavbarItems from "./NavbarItems/NavbarItems";
 
 function Navbar() {
-	return <div>Navbar</div>;
+	return (
+		<NavbarStyled>
+			<NavbarItems />
+		</NavbarStyled>
+	);
 }
 
 export default Navbar;
+
+const NavbarStyled = styled.nav`
+	width: 100%;
+	height: 50px;
+	position: sticky;
+	top: 0px;
+	z-index: 999;
+`;
---
+++ src/Components/Navbar/NavbarItems/NavbarItems.jsx
@@ -0,0 +1,110 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { SectionOnScreenContext } from "../../../Context/sectionOnScreen";
+
+function NavbarItems() {
+	const {
+		home,
+		about,
+		software,
+		petroleum,
+		toggleHome,
+		toggleAbout,
+		toggleSoftware,
+		togglePetroleum,
+		toggleContact,
+	} = useContext(SectionOnScreenContext);
+	return (
+		<Ul>
+			<Li onClick={toggleHome} border={home}>
+				<ALink>Home_</ALink>
+			</Li>
+			<Li onClick={toggleAbout} border={about}>
+				<ALink>About ME_</ALink>
+			</Li>
+			<Li onClick={toggleSoftware} border={software}>
+				<ALink>Software Testing_</ALink>
+			</Li>
+			<Li onClick={togglePetroleum} border={petroleum}>
+				<ALink>Petroleum Engineering_</ALink>
+			</Li>
+			<Buttons bgColor='transparent'>Resume</Buttons>
+			<Buttons border='none' color='white' onClick={toggleContact}>
+				Contact
+			</Buttons>
+		</Ul>
+	);
+}
+
+export default NavbarItems;
+
+const Ul = styled.ul`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: row;
+
+	justify-content: flex-end;
+	align-items: center;
+
+	padding: 0px 30px;
+
+	flex-wrap: nowrap;
+	gap: 60px;
+
+	background-color: var(--bodyBackgroundColor);
+`;
+
+const ALink = styled.a`
+	text-decoration: none;
+	cursor: pointer;
+`;
+
+const Li = styled.li`
+	width: 207px;
+	height: 35px;
+	line-height: 35px;
+	list-style: none;
+	color: var(--orangeStrong);
+	font-family: var(--sanchezFont);
+	font-weight: 400;
+	font-size: 18px;
+	text-align: center;
+	cursor: pointer;
+	border-bottom: ${({ border }) =>
+		border ? "1px solid var(--orangeStrong)" : "none"};
+	position: relative;
+
+	::before {
+		content: "";
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		width: 0;
+		height: 1px;
+		background-color: var(--orangeStrong);
+		transition: width 0.6s cubic-bezier(0.25, 1, 0.5, 1);
+	}
+
+	:hover::before {
+		left: 0;
+		right: auto;
+		width: 100%;
+	}
+`;
+
+const Buttons = styled.button`
+	width: 154px;
+	height: 38px;
+	border-radius: 20px;
+
+	font-family: var(--robotoFont);
+	font-weight: 400;
+	font-size: 24px;
+
+	color: ${({ color }) => color || "var(--orangeStrong)"};
+	background-color: ${({ bgColor }) => bgColor || "var(--orangeStrong)"};
+	border: ${({ border }) => border || "3px solid var(--orangeStrong)"};
+
+	cursor: pointer;
+`;
