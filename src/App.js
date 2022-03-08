fe9baecd67076d84589a7b000c2aa588f122de0b
Smael Nicolas <nico.smael@gmail.com>
2022-03-08 18:13:58 -0300

    change section x pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import Navbar from "./Components/Navbar/Navbar";
 import styled from "styled-components";
-import Home from "./Pages/Home/Home";
-import About from "./Pages/About/About";
-import PetroleumEngineering from "./Pages/PetroleumEngineering/PetroleumEngineering";
-import SoftwareTesting from "./Pages/SoftwareTesting/SoftwareTesting";
-import ContactMe from "./Pages/ContactMe/ContactMe";
 import IconsMedia from "./Components/IconsMedia/IconsMedia";
+import Pages from "./Pages/Pages";
 
 function App() {
 	return (
 		<AppStyled>
 			<Navbar />
 			<IconsMedia />
-			<Home />
-			<About />
-			<PetroleumEngineering />
-			<SoftwareTesting />
-			<ContactMe />
+			<Pages />
 		</AppStyled>
 	);
 }
@@ -26,9 +18,6 @@ export default App;
 const AppStyled = styled.div`
 	// width para evitar tener en cuenta la barra de scroll en el ancho . Width 100% seria hasta donde empieza el scroll
 	width: calc(100vw- (100vw - 100%));
+	height: 100vh;
 	position: relative;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
 `;
