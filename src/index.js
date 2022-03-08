d4d8110ed903addb411472f9d9867ed398d3fee6
Smael Nicolas <nico.smael@gmail.com>
2022-03-08 13:53:27 -0300

    Global variables & styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+const GlobalStyle = createGlobalStyle`
+
+     :root {
+        --orangeLight : #F8971F;
+        --orangeLightSecond : #f8961f62;
+        --orangeStrong : #BF5700;
+        --grey :#808080;
+        --greyLight :#8080807e;
+        --bodyBackgroundColor : #E5E5E5;
+        --robotoFont : 'Roboto', sans-serif;
+        --trispaceFont: 'Trispace', sans-serif;
+		--sanchezFont:'Sanchez', serif;
+     }
+
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        scroll-behavior: smooth;
+        cursor: default ;
+    }
+
+    body{
+        background-color:var(--bodyBackgroundColor) ;
+    }
+`;
+
 ReactDOM.render(
 	<React.StrictMode>
+		<GlobalStyle />
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
