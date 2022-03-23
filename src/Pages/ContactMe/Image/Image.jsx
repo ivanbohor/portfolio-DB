7666e4abbebe94cd431694b21ed1b15d798c7cb9
Smael Nicolas <nico.smael@gmail.com>
2022-03-23 13:50:40 -0300

    new img, text descrip

--- src/Pages/ContactMe/Image/Image.jsx
+++ src/Pages/ContactMe/Image/Image.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import styled, { keyframes } from "styled-components";
 
 import imgAsset from "../../../Assets/contactImage.svg";
+import imgLogo from "../../../Assets/logoDaniel.svg";
 
 function Image() {
-	return <ImgContact src={imgAsset} />;
+	return (
+		<ImageContainer>
+			<ImgContact src={imgAsset} />
+			<Logo src={imgLogo} />
+		</ImageContainer>
+	);
 }
 
 export default Image;
@@ -16,7 +22,27 @@ const Fade = keyframes`
 `;
 
 const ImgContact = styled.img`
+	width: 100%;
+`;
+const ImageContainer = styled.div`
 	width: 70%;
 	opacity: 0;
 	animation: ${Fade} 3s forwards;
+	position: relative;
+`;
+
+const Logo = styled.img`
+	width: 50%;
+	opacity: 0;
+	animation: ${Fade} 3s forwards;
+	position: absolute;
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	margin-left: auto;
+	margin-right: auto;
+	margin-top: auto;
+	margin-bottom: auto;
 `;
